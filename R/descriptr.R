e55cef7a0f09ec4048aaa8ac32251816dbb2f04b
rsquaredin <rsquaredin@gmail.com>
2017-11-09 02:06:19 +0530

    add descriptr.R file

---
+++ R/descriptr.R
@@ -0,0 +1,11 @@
+#' \code{descriptr} package
+#'
+#' Generate descriptive statistics and explore statistical distributions
+#'
+#' @docType package
+#' @name descriptr
+NULL
+
+## quiets concerns of R CMD check re: the .'s that appear in pipelines
+if(getRversion() >= "2.15.1")  utils::globalVariables(c(".", "breaks",
+  "cumulative", "frequency"))
