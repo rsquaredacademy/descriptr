59c5de02c340b48e1ae08607f5e03f7b230411d5
rsquaredin <rsquaredin@gmail.com>
2017-12-11 18:00:12 +0530

    update demo video link

--- inst/application/ui/ui_transform2.R
+++ inst/application/ui/ui_transform2.R
@@ -9,7 +9,7 @@ tabPanel('Transform', value = 'tab_transform',
                  ),
                  column(6, align = 'right',
                    actionButton(inputId='translink3', label="Demo", icon = icon("video-camera"),
-                     onclick ="window.open('http://google.com', '_blank')")
+                     onclick ="window.open('https://www.youtube.com/watch?v=IckaPr19Bvc#t=01m23s', '_blank')")
                  )
                ),
                hr(),
