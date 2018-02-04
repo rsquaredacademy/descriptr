9d5027f141a6b6d0a0e1a46ff07c5263de8795ce
rsquaredin <rsquaredin@gmail.com>
2018-02-04 21:45:30 +0530

    add global variables for cran check

--- R/ds-descriptr.R
+++ R/ds-descriptr.R
@@ -8,4 +8,5 @@ NULL
 
 ## quiets concerns of R CMD check re: the .'s that appear in pipelines
 if(getRversion() >= "2.15.1")  utils::globalVariables(c(".", "breaks",
-  "cumulative", "frequency", "values", 's', 'tp', 'type'))
+  "cumulative", "frequency", "values", 's', 'tp', 'type', "xvar", "yvar",
+  "data", "varnames", "frequency", "varname", "bins"))
