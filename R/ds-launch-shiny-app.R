cb6ce258dade15f06810150da5248744f66ea981
rsquaredin <rsquaredin@gmail.com>
2019-01-14 16:12:30 +0530

    closes #76

--- R/ds-launch-shiny-app.R
+++ R/ds-launch-shiny-app.R
@@ -11,8 +11,22 @@
 #' @export
 #'
 ds_launch_shiny_app <- function() {
-  rlang::abort("The shiny app has been moved to a new package, `xplorerr`. To launch the app, run the below code:\n 
-	- install.packages('xplorerr')\n - xplorerr::app_descriptive()")
+
+	rlang::inform("`ds_launch_shiny_app()` has been soft-deprecated and will be removed in the next release. In future, to launch the app, run the below code:\n 
+	- install.packages('xplorerr')\n - xplorerr::app_descriptive()\n")
+
+	check_suggests('haven')
+	check_suggests('jsonlite')
+	check_suggests('readr')
+	check_suggests('readxl')
+	check_suggests('shinyBS')
+	check_suggests('shinycssloaders')
+	check_suggests('shinythemes')
+	check_suggests('stringr')
+	check_suggests('lubridate')
+
+	xplorerr::app_descriptive()
+
 }
  
 #' @export
