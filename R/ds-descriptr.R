34315d19ce689e4c2f4589e850f9aaa84e26f43e
rsquaredin <rsquaredin@gmail.com>
2019-10-03 22:54:59 +0530

    fix undefined global variable note

--- R/ds-descriptr.R
+++ R/ds-descriptr.R
@@ -9,8 +9,9 @@ NULL
 ## quiets concerns of R CMD check re: the .'s that appear in pipelines
 if (getRversion() >= "2.15.1") {
   utils::globalVariables(c(
-    ".", "breaks", "Levels",
+    ".", "breaks", "Levels", "varyable",
     "cumulative", "frequency", "values", "s", "tp", "type", "xvar", "yvar",
-    "data", "varnames", "frequency", "varname", "bins", "n", "value", "y", "count"
+    "data", "varnames", "frequency", "varname", "bins", "n", "value", "y", "
+    count"
   ))
 }
